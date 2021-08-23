881e9227fbf7a4f6b2fe8205dcf598efbdcf6bdb
Francisco Montoto <ezejerez99@gmail.com>
2021-08-22 23:12:30 -0300

    ..

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import {React, useState} from 'react';
 import './input.css';
 
 export default function Input() {
-  return <input />;
+
+  const [inputValue, setInputValue] = useState('')
+
+  function inputResult(){
+    return 
+  }
+
+  function keyPress(e) {
+    if (e.keyCode === 13){
+       console.log('value', e.target.value);
+       // put the login here
+    }
+ }
+
+  return <input onKeyDown={keyPress} onChange={event => setInputValue(event.target.value)}/>;
 }
